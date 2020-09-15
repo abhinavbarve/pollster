6fa7e6c88e747b048b8e729b40c28462f98cf75a
abhinavbarve <abhinav.barve2001@gmail.com>
2020-09-15 19:30:31 +0530

    create navbar

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(body_parser.urlencoded({extended : true}))
 
 // declare public files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 
 
@@ -30,8 +30,8 @@ app.get("/" , (req,res)=>{
 
 
 // Listen on Port 3000
-app.listen(3000, (req,res)=>{
-  console.log("Server is running on port 3000.")
+app.listen(8080, (req,res)=>{
+  console.log("Server is running on port 8080.")
 })
 
 
