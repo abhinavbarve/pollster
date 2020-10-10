fbc2addaa3613af6a9f0655910e74e3f69ab04f3
abhinavbarve <abhinav.barve2001@gmail.com>
2020-10-10 18:49:46 +0530

    make website responsive

--- public/javascripts/frontend.js
+++
@@ -1,5 +0,0 @@
-let width = window.innerWidth
-if (width <= 800) {
-    document.getElementById("new-ques").innerHTML = "<strong>Ques</strong>"
-    document.getElementById("new-options").innerHTML = "<strong>Options</strong>" 
-}
